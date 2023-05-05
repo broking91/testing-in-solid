4b815092bfe5ce62f9603ef92f50125ee7852088
broking91 <122006116+broking91@users.noreply.github.com>
2023-05-05 12:34:29 +0530

    Update Counter.jsx

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,16 @@
 // example Counter from Solid js
-import { createSignal, onCleanup } from "solid-js";
+import { createSignal } from "solid-js";
 
 const Counter = () => {
 	const [count, setCount] = createSignal(0);
-	const interval = setInterval(
-		() => setCount(c => c + 1),
-		1000
-	);
-	onCleanup(() => clearInterval(interval));
-	return <div>Count value is <span>{count()}</span></div>;
+	
+	return <div>
+        Count value is <span >{count()}</span>
+        <button onClick={() => setCount((c) => c + 1)}>
+            Increment
+        </button>
+    
+        </div>;
 };
 
 export default Counter;
